ce14806926980434f9469672970c09710e942476
abraham <abe@unscene.us>
2016-07-08 14:53:35 -0700

    Removed description when choosing a social network

--- extensions/default/templates/UserSettings.php
+++ extensions/default/templates/UserSettings.php
@@ -7,18 +7,21 @@
         }
     ?>
 
-            <ul class="nav">
+            <div class="nav_unused">
                 <?php
                     //get the active
+                    $varsdfsdfsdf = $_GET["network"];
                     $buddyStreamExtension = new BuddyStreamExtensions();
                     foreach ($buddyStreamExtension->getExtensionsConfigs() as $extension) {
                         if (get_site_option('buddystream_' . $extension['name'] . '_power') == "on" && get_site_option('buddystream_' . $extension['name'] . '_setup') && !$extension['parent']) {
-                            echo '<li><a href="?network=' . $extension['name'] . '" id="' . ucfirst($extension['displayname']) . '">' . ucfirst($extension['displayname']) . '</a></li>';
+                            echo '<div><a class="btn waves-effect';
+                            if ($extension['name'] == $varsdfsdfsdf) {echo ' green';}
+                            echo '" href="?network=' . $extension['name'] . '" id="' . ucfirst($extension['displayname']) . '">' . ucfirst($extension['displayname']) . '</a></div>';
                             $activeExtensions[] = $extension['name'];
                         }
                     }
                 ?>
-            </ul>
+            </div>
 
     <br/>
 
